1b082baae54f75e0ed00395b5388455871e2c5aa
st1807 <1807220320@qq.com>
2020-11-27 17:30:38 +0800

    add terrain and human run

---
+++ Assets/GitData/Scripts/CameraController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+    private void Awake()
+    {
+        instance=this;
+    }
+    Transform myTrans;
+    Transform targetTrans;
+    Transform targetPivot;
+
+    float rotateSpeed = 5;
+    float followSpeed= 6;
+
+    float smoothX, smoothY , smoothVelocityX ,smoothVelocityY;
+    float angleY , angleX;
+
+    float smoothNum = 0.5f;
+    
+    void Start()
+    {
+        myTrans= transform;
+    }
+
+    public void BindTarget(Transform _target)
+    {
+        targetTrans = _target;
+        targetPivot = transform.Find("Pivot");
+    }
+
+    public void FixedRotate()
+    {
+        if (targetTrans==null)
+        {
+            return;
+        }
+        float rotateX = Input.GetAxis("Mouse X");
+        float rotateY = Input.GetAxis("Mouse Y");
+        HandleRotation(rotateX , rotateY);
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        Vector3 pos= Vector3.Lerp(myTrans.position,targetTrans.position,Time.fixedDeltaTime * followSpeed);
+        myTrans.position = pos;
+    }
+    private void HandleRotation(float _ver, float _hor)
+    {
+        smoothX = Mathf.SmoothDamp(smoothX , _ver, ref smoothVelocityX, smoothNum);
+        smoothY = Mathf.SmoothDamp(smoothY , _hor, ref smoothVelocityY, smoothNum);
+
+        
+        angleX += smoothX * rotateSpeed;
+        myTrans.rotation = Quaternion.Euler(0 , angleX , 0);
+
+        if (targetPivot!=null)
+        {
+            angleY -= smoothY * rotateSpeed;
+            angleY = Mathf.Clamp(angleY ,-45f ,45f);
+            targetPivot.localRotation = Quaternion.Euler(angleY , 0 , 0);
+            // Debug.Log("rotation : "+targetPivot.rotation);
+        }
+
+    }
+}
---
+++ Assets/GitData/Scripts/CharactorsManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactorsManager : MonoBehaviour
+{
+    // Collider humanCollider;
+    Camera controlCamera;
+    Rigidbody humanRigid;
+    Vector3 moveX,moveY , moveDir;
+
+    GameObject charactorObj;
+
+    Animator animator;
+
+    float vertical,horizontal;
+    float basicSpeed = 2;
+    float speedPower = 1;
+
+    float moveLimit=0;
+
+    Quaternion moveRotation;
+    bool canMove = false;
+
+    bool isJump;
+
+    bool isRoll;
+
+    bool isOnGround=true;
+
+    bool isSpeedUp;
+
+    bool isDizzy;
+
+    Transform myTrans;
+
+    CameraController cameraController;
+
+    void Start()
+    {
+        // humanCollider=GetComponent<Collider>();
+        myTrans=transform;
+        humanRigid=GetComponent<Rigidbody>();
+
+        controlCamera=Camera.main;
+
+        charactorObj=transform.Find("charactor").gameObject;
+        animator=charactorObj.GetComponent<Animator>();
+
+        if (moveRotation==null)
+        {
+            moveRotation= Quaternion.LookRotation(myTrans.forward);
+        }
+
+        cameraController = CameraController.instance;
+        cameraController.BindTarget(myTrans);
+
+    }
+
+    void FixedUpdate()
+    {
+        FixedMove();
+
+        cameraController.FixedRotate();
+    }
+    void Update()
+    {
+        UpdateData();
+        UpdateState();
+    }
+
+    void UpdateData()
+    {
+        vertical=Input.GetAxis("Vertical");
+        horizontal=Input.GetAxis("Horizontal");
+    }
+
+    void UpdateState()
+    {
+
+        // moveX = vertical * transform.forward;
+        // moveY = horizontal * transform.right;
+        moveX = vertical * controlCamera.transform.forward;
+        moveY= horizontal * controlCamera.transform.right;
+        float moveSpeed= basicSpeed * speedPower;
+
+        moveDir = ((moveX + moveY).normalized) * moveSpeed * moveLimit;
+        moveDir.y=humanRigid.velocity.y;
+
+        float m = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+        moveLimit = Mathf.Clamp01(m);
+
+    }
+
+    void FixedMove()
+    {
+
+        if (moveDir.x!=0 ||moveDir.z!=0)
+        {
+            // Debug.Log(moveDir);
+
+            animator.SetBool("canMove",true);
+            canMove=true;
+        }
+        else
+        {
+            animator.SetBool("canMove",false);
+            canMove=false;
+        }
+
+        // canMove=animator.GetBool("canMove");
+        if (canMove)
+        {
+            humanRigid.velocity=moveDir;// human move;
+
+            Quaternion targetQua= Quaternion.LookRotation(moveDir);
+            moveRotation = Quaternion.Slerp(myTrans.rotation,targetQua,Time.fixedDeltaTime * 5);
+            myTrans.rotation=moveRotation;
+        }
+
+    }
+}
